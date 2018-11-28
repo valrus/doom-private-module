3c451c97a98bb96aa6ba1be2ef8e4dbc611b46c2
Ian McCowan <ian@bonanza.com>
2018-11-28 11:55:24 -0800

    disable magit-todos
    idk how long this takes to run but I don't want it

--- init.el
+++ init.el
@@ -155,7 +155,8 @@
 
 ;; To disable a package (tells Doom to ignore def-package! blocks for this
 ;; package)
-;; (def-package-hook! evil-goggles :disable)
+
+(def-package-hook! magit-todos :disable)
 
 ;; If a :pre-init / :pre-config hook returns nil, it overwrites that package's
 ;; original :init / :config block. Exploit this to overwrite Doom's config.
