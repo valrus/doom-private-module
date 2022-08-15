38c29f63acd7768c8287af9886818cfd4caf2a38
Ian McCowan <ian.mccowan@joinhonor.com>
2022-08-15 09:04:16 -0700

    add some helpers

--- bindings/+code.el
+++ bindings/+code.el
@@ -23,7 +23,27 @@
   :desc "Fold top level" :n [return] #'hs-hide-all
   :desc "Toggle hiding" :n "SPC" #'hs-toggle-hiding))
 
+(defun valrus/py-fold-class ()
+  (interactive)
+  (hs-hide-level 2))
+
 (map!
  :map python-mode-map
  (:prefix "z"
   :desc "Fold class" :n "C" #'hs-hide-level))
+
+(map!
+ (:after eglot
+  (:leader
+   (:prefix "c"
+    :desc "Toggle event logging" :n "l" #'valrus/toggle-eglot-event-log))))
+
+(map!
+  (:leader
+   (:prefix "c"
+    (:prefix ("~" . "Change string inflection")
+     :desc "snake_case" :nv "s" #'string-inflection-underscore
+     :desc "PascalCase" :nv "p" #'string-inflection-pascal-case
+     :desc "kebab-case" :nv "k" #'string-inflection-kebab-case
+     :desc "SCREAMING_SNAKE_CASE" :nv "S" #'string-inflection-upcase
+     :desc "camelCase" :nv "c" #'string-inflection-camelcase))))
