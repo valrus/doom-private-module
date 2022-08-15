4106aa4e6e3f5f6389a57630afbdb3e465926381
Ian McCowan <ian.mccowan@joinhonor.com>
2022-08-15 09:04:40 -0700

    add easymotion helpers

--- bindings/+easymotion.el
+++ bindings/+easymotion.el
@@ -3,4 +3,7 @@
 (map!
  (:prefix "g"
   :desc "Easymotion up" :nv [up] #'evilem-motion-previous-line
-  :desc "Easymotion down" :nv [down] #'evilem-motion-next-line))
+  :desc "Easymotion down" :nv [down] #'evilem-motion-next-line
+  :desc "Avy goto char" :nv "SPC" #'avy-goto-char-timer
+  (:prefix "s"
+   :desc "Avy goto line" :nv "l" #'avy-goto-line)))
