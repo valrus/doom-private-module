c8ac70fc7c33f2ea3eaa033c543fcfcc6372d2b9
valrus <imccowan@gmail.com>
2022-08-15 18:05:11 -0700

    add some font choices

--- local/+imac-home.el
+++ local/+imac-home.el
@@ -18,12 +18,22 @@
  ;; doom-font (font-spec :family "Inconsolata" :size 14 :width 'semi-condensed)
  ;; doom-font (font-spec :family "Operator Mono Light" :weight 'light :size 14)
  ;; doom-font (font-spec :family "Iosevka" :size 14 :weight 'semi-light)
- doom-font (font-spec :family "Iosevka" :size 14 :weight 'semi-light)
+
+ ;; fonts: iosevka custom
+ doom-font (font-spec :family "Iosevka Custom" :size 14 :weight 'light)
  doom-serif-font (font-spec :family "Iosevka Slab" :size 14 :weight 'semi-light)
- doom-unicode-font (font-spec :family "Iosevka" :size 14 :weight 'semi-light)
- doom-big-font (font-spec :family "Iosevka" :size 20 :weight 'normal)
+ doom-unicode-font (font-spec :family "Iosevka Custom" :size 14 :weight 'light)
+ doom-big-font (font-spec :family "Iosevka Custom" :size 20 :weight 'light)
  doom-variable-pitch-font (font-spec :family "PT Sans" :weight 'light :size 14)
- minibuffer-font-spec '((default :family "Iosevka"))
+ minibuffer-font-spec '((default :family "Iosevka Custom"))
+
+ ;; fonts: cartograph
+ ;; doom-font (font-spec :family "Cartograph Mono CF" :size 14 :weight 'light)
+ ;; doom-serif-font (font-spec :family "Cartograph Mono CF" :size 14 :weight 'semi-light)
+ ;; doom-unicode-font (font-spec :family "Cartograph Mono CF" :size 14 :weight 'light)
+ ;; doom-big-font (font-spec :family "Cartograph Mono CF" :size 20 :weight 'light)
+ ;; doom-variable-pitch-font (font-spec :family "PT Sans" :weight 'light :size 14)
+ ;; minibuffer-font-spec '((default :family "Cartograph Mono CF"))
 
  ;; theme
  doom-theme 'doom-nord)
