acd7b865f518f28a4aea1ed19b7673d09c92eb79
valrus <ian@mccowan.space>
2023-07-21 22:03:41 -0700

    gallium font changes

--- local/+gallium.el
+++ local/+gallium.el
@@ -4,12 +4,12 @@
  show-trailing-whitespace nil
 
  ;; Fonts
- doom-font (font-spec :family "Iosevka Term" :size 14)
- doom-serif-font (font-spec :family "Iosevka Term Slab" :size 14)
- doom-unicode-font (font-spec :family "Iosevka Term")
- doom-big-font (font-spec :family "Iosevka Term" :size 20)
- doom-variable-pitch-font (font-spec :family "Iosevka Term")
- minibuffer-font-spec '((default :family "Iosevka Term" :height 1.2))
+ doom-font (font-spec :family "Iosevka Comfy" :size 14)
+ doom-serif-font (font-spec :family "Iosevka Comfy Motion" :size 14)
+ doom-unicode-font (font-spec :family "Iosevka Comfy")
+ doom-big-font (font-spec :family "Iosevka Comfy" :size 20)
+ doom-variable-pitch-font (font-spec :family "Iosevka Comfy")
+ minibuffer-font-spec '((default :family "Iosevka Comfy" :height 1.2))
 
  ;; theme
  doom-theme 'doom-nord
@@ -18,12 +18,12 @@
  x-select-enable-clipboard-manager nil)
 
 ;; line numbers must be monospaced
-(set-face-attribute 'line-number nil :family "Iosevka Term")
-(set-face-attribute 'line-number-current-line nil :family "Iosevka Term")
+(set-face-attribute 'line-number nil :family "Iosevka Comfy")
+(set-face-attribute 'line-number-current-line nil :family "Iosevka Comfy")
 
 ;; minibuffer
-(set-face-attribute 'minibuffer-prompt nil :family "Iosevka Term" :weight 'bold)
+(set-face-attribute 'minibuffer-prompt nil :family "Iosevka Comfy" :weight 'bold)
 
 (after! ivy-posframe
   (setq-default
-   ivy-posframe-font (font-spec :family "Iosevka Term" :size 14)))
+   ivy-posframe-font (font-spec :family "Iosevka Comfy" :size 14)))
