4b52aa5cea2f0205707f949ecde3c6bbce9fff31
valrus <ian@mccowan.space>
2023-11-24 13:13:22 -0800

    fix merge error

--- packages.el
+++ packages.el
@@ -15,10 +15,7 @@
 (package! vimrc-mode)
 (package! json-process-client :recipe (:host github :repo "emacsmirror/json-process-client"))
 (package! ef-themes)
-<<<<<<< HEAD
-=======
 
->>>>>>> 4be46cb (make some stuff work only)
 (when (local-config-work-p)
   (package! chatgpt-shell
     :recipe (:host github :repo "xenodium/chatgpt-shell"))
