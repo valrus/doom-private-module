890f4142c1133e800ca28b35b891207ce1289851
Ian McCowan <ian.mccowan@joinhonor.com>
2025-04-01 08:43:32 -0700

    copilot bindings, dupe removal

--- bindings/+code.el
+++ bindings/+code.el
@@ -41,13 +41,21 @@
   (:prefix "z"
    :desc "Hide class" :n "C" #'hs-hide-level)
   (:localleader
-   :desc "Update Optional syntax" :n "o" #'valrus/py-convert-optional-typing)))
+   :desc "Update Optional syntax" :n "o" #'valrus/py-convert-optional-typing
+   (:prefix "t"
+    :desc "Run test at point" :n "t" #'python-pytest-run-def-or-class-at-point-dwim))))
 
 (map!
  (:after eglot
          (:leader
           (:prefix "c"
            :desc "Toggle event logging" :n "l" #'valrus/toggle-eglot-event-log))))
+
+(map!
+ (:after chatgpt-shell
+         (:leader
+          (:prefix "c"
+           :desc "LLM compose" :n "g" #'chatgpt-shell-prompt-compose))))
 
 (map!
  (:leader
@@ -65,37 +73,6 @@
   :nv "R" #'+lookup/references
   :nv "D" #'valrus/goto-def-ace-window))
 
-(defun valrus/copilot-change-activation ()
-  "Switch between three activation modes:
-- automatic: copilot will automatically overlay completions
-- manual: you need to press a key to trigger completions
-- off: copilot is completely disabled."
-  (interactive)
-  (if (and copilot-mode valrus/copilot-manual-mode)
-      (progn
-        (message "deactivating copilot")
-        (global-copilot-mode -1)
-        (setq valrus/copilot-manual-mode nil))
-    (if copilot-mode
-        (progn
-          (message "activating copilot manual mode")
-          (setq valrus/copilot-manual-mode t))
-      (message "activating copilot mode")
-      (global-copilot-mode))))
-
-(map!
- ;; explicit copilot complete
- :desc "Copilot complete" :i "M-<return>" #'copilot-complete
- :desc "Copilot activation toggle" "M-<escape>" #'valrus/copilot-change-activation
- ;; accept completion from copilot and fallback to company
- (:map copilot-completion-map
-       "M-<right>" #'copilot-accept-completion-by-word
-       "M-<down>" #'copilot-accept-completion-by-word
-       "M-p" #'copilot-previous-completion
-       "M-n" #'copilot-next-completion
-       "M-<return>" #'copilot-accept-completion
-       "C-g" #'copilot-clear-overlay))
-
 (defun valrus/pytest-copy-test-name ()
   (interactive)
   (let ((test-name (format "%s::%s" (buffer-file-name) (python-pytest--current-defun))))
@@ -110,12 +87,6 @@
    :desc "Copy test name" :n "c" #'valrus/pytest-copy-test-name)))
 
 (when (local-config-work-p)
-  (defun valrus/clear-copilot-and-delete ()
-    (interactive)
-    (copilot-clear-overlay)
-    (evil-delete-backward-char)
-    (copilot-clear-overlay))
-
   (defun valrus/copilot-change-activation ()
     "Switch between three activation modes:
 - automatic: copilot will automatically overlay completions
